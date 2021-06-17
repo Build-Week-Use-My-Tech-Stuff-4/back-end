36d66b1e0336a9f87e3b8145e0ed3da1b04098dd
Tony Sorensen <tony48853@gmail.com>
2021-06-17 02:05:08 -0700

    more file structure, server/route setup

--- api/server.js
+++ api/server.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const authRouter = require("./auth/auth-router");
+const usersRouter = require("./users/users-router");
+const itemsRouter = require("./items/items-router");
 const server = express();
-server.use(express.json());
+
+server.use(helmet());
+server.use(express.json())
+server.use(cors())
+
+
+server.use("/api/auth", authRouter)
+server.use("/api/users", usersRouter)
+server.use("/api/items", itemsRouter)
+
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: process.env.MOTD });
 });
+
+
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    });
+  });
 
 module.exports = server;
