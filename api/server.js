e10fa522a4df174ae464dccb3e0bc559911765d5
Tony Sorensen <tony48853@gmail.com>
2021-06-17 01:42:02 -0700

    updated read me

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const server = express();
 server.use(express.json());
 
 server.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to the Use My Tech Stuff Build" });
+  res.status(200).json({ message: process.env.MOTD });
 });
 
 module.exports = server;
