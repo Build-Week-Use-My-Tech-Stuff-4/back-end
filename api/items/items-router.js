dc3d403ab38d043a800331d1f3b8e6b249185083
Tony Sorensen <tony48853@gmail.com>
2021-06-21 18:52:11 -0700

    user routes working

--- api/items/items-router.js
+++ api/items/items-router.js
@@ -1,2 +1,27 @@
 const router = require("express").Router();
+const Items = require("./items-model");
+
+router.get("/", (req, res, next) => {
+  Items.findAll()
+    .then((items) => {
+      res.status(200).json(items);
+    })
+    .catch(next);
+});
+
+router.get("/:id", (req, res, next) => {
+  Items.findById(req.params.id)
+    .then((item) => {
+      res.status(200).json(item);
+    })
+    .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+  Items.add(req.body).then((item) => {
+    res.status(201).json(item);
+  })
+  .catch(next)
+});
+
 module.exports = router;
