2a9accfaad83127903448d248760e6f9d70b4e86
Tony Sorensen <tony48853@gmail.com>
2021-06-23 01:37:12 -0700

    item model findById

--- api/items/items-model.js
+++ api/items/items-model.js
@@ -9,52 +9,53 @@ module.exports = {
   update,
 };
 
-async function findAll() {
-  const itemsWithReviews = await db("items")
-    .select(
-      "items.item_id",
-      "items.item_name",
-      "items.description",
-      "items.location",
-      "items.price_per_day",
-      "items.available",
-      "items.lister_id",
-      "reviews.review_id",
-      "reviews.reviewed_item_id",
-      "reviews.reviewer_id",
-      "reviews.review_text",
-      "reviews.stars"
-    )
-    .leftJoin("reviews", "items.item_id", "reviews.reviewed_item_id");
-
-  if (!itemsWithReviews.length) return null;
-
-  const item = {
-    item_id: itemsWithReviews[0].item_id,
-    item_name: itemsWithReviews[0].item_name,
-    description: itemsWithReviews[0].description,
-    location: itemsWithReviews[0].location,
-    price_per_day: itemsWithReviews[0].price_per_day,
-    available: itemsWithReviews[0].available,
-    lister_id: itemsWithReviews[0].lister_id,
-    reviews: [],
-  };
-  const reviewsArray = [];
-
-  itemsWithReviews.forEach((review) => {
-    reviewsArray.push({
-      review_id: review.review_id,
-      review_text: review.review_text,
-      stars: review.stars,
-      reviewer_id: review.review_id,
-      reviewed_item_id: review.reviewed_item_id,
-    });
-  });
-  return { ...item, reviews: reviewsArray };
+function findAll() {
+ return db("items")
 }
 
-function findById(id) {
-  return db("items").where({ item_id: id }).first();
+async function findById(id) {
+const itemsWithReviews = await db("items")
+.select(
+  "items.item_id",
+  "items.item_name",
+  "items.description",
+  "items.location",
+  "items.price_per_day",
+  "items.available",
+  "items.lister_id",
+  "reviews.review_id",
+  "reviews.reviewed_item_id",
+  "reviews.reviewer_id",
+  "reviews.review_text",
+  "reviews.stars"
+)
+.leftJoin("reviews", "items.item_id", "reviews.reviewed_item_id")
+.where("items.item_id", id)
+
+if (!itemsWithReviews.length) return null;
+
+const item = {
+item_id: itemsWithReviews[0].item_id,
+item_name: itemsWithReviews[0].item_name,
+description: itemsWithReviews[0].description,
+location: itemsWithReviews[0].location,
+price_per_day: itemsWithReviews[0].price_per_day,
+available: itemsWithReviews[0].available,
+lister_id: itemsWithReviews[0].lister_id,
+reviews: [],
+};
+const reviewsArray = [];
+
+itemsWithReviews.forEach((review) => {
+reviewsArray.push({
+  review_id: review.review_id,
+  review_text: review.review_text,
+  stars: review.stars,
+  reviewer_id: review.review_id,
+  reviewed_item_id: review.reviewed_item_id,
+});
+});
+return { ...item, reviews: reviewsArray };
 }
 
 function findBy(filter) {
@@ -72,3 +73,5 @@ async function add(item) {
 function update(id, changes) {
   return db("items").where({ id }).update(changes, "*");
 }
+
+
